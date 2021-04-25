8b0080dae0050a76dc87ec7c69519ead71d234a5
inokawa <48897392+inokawa@users.noreply.github.com>
2021-04-25 10:34:10 +0900

    Impl sample4

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,3 +1,5 @@
+var squareRotation = 0.0;
+
 main();
 
 //
@@ -70,8 +72,19 @@ function main() {
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
-  // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  var then = 0;
+
+  // Draw the scene repeatedly
+  function render(now) {
+    now *= 0.001; // convert to seconds
+    const deltaTime = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, deltaTime);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 //
@@ -102,7 +115,7 @@ function initBuffers(gl) {
 
   // Now set up the colors for the vertices
 
-  var colors = [
+  const colors = [
     1.0,
     1.0,
     1.0,
@@ -134,7 +147,7 @@ function initBuffers(gl) {
 //
 // Draw the scene.
 //
-function drawScene(gl, programInfo, buffers) {
+function drawScene(gl, programInfo, buffers, deltaTime) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
   gl.clearDepth(1.0); // Clear everything
   gl.enable(gl.DEPTH_TEST); // Enable depth testing
@@ -173,6 +186,12 @@ function drawScene(gl, programInfo, buffers) {
     modelViewMatrix, // matrix to translate
     [-0.0, 0.0, -6.0]
   ); // amount to translate
+  mat4.rotate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to rotate
+    squareRotation, // amount to rotate in radians
+    [0, 0, 1]
+  ); // axis to rotate around
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
@@ -236,6 +255,10 @@ function drawScene(gl, programInfo, buffers) {
     const vertexCount = 4;
     gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
   }
+
+  // Update the rotation for the next draw
+
+  squareRotation += deltaTime;
 }
 
 //
